673800ff69b5cbf7a07b93dc1c9457413decdb63
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2020-11-26 16:56:10 +0100

    Emacs

--- func.el
+++ func.el
@@ -2,3 +2,8 @@
  "Prints todays date and time in ER format"
  (interactive)
  (insert (format-time-string "%a, %Y-%m-%d: %H:")))
+
+(defun er/backup ()
+  "Backup org and emacs files"
+  (interactive)
+  (shell-command "sh ~/bin/backup.sh"))
