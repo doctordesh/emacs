dccd964ff8b7d04c42a59bff7517b8bd01df12cd
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2020-12-04 08:00:08 +0100

    Emacs

--- func.el
+++ func.el
@@ -7,3 +7,15 @@
   "Backup org and emacs files"
   (interactive)
   (start-process-shell-command "backup" "*Messages*" "sh ~/bin/backup.sh"))
+
+
+(setq projects_dir "~/Development")
+(defun er/clone (repo),,
+  "Clone repo from KITS SW Gitlab and put it in the right place"
+  (interactive "sWhich repo? (gitlab.maxiv.lu.se/kits-maxiv/ ")
+  (setq repo-url (format "git@gitlab.maxiv.lu.se:kits-maxiv/%s" repo))
+  (setq download-dir (expand-file-name (format "%s/%s" projects_dir repo))
+  (message "Repo: %s" repo-url)
+  (message project_dir)
+  (message download-dir)
+  )
