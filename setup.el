690bd199949ab77d8bce405d920d6bc3ffc453ec
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2020-12-16 09:00:08 +0100

    Emacs

--- setup.el
+++ setup.el
@@ -61,6 +61,7 @@
 ;; Show time
 (display-time-mode 1)
 (setq display-time-24hr-format 1)
+(setq display-time-default-load-average nil)
 
 ;; Don't as yes or no, but y or n
 (fset 'yes-or-no-p 'y-or-n-p)
