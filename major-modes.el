08474aea634cef3ab4f19405761edd0689797d8c
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2021-05-10 10:00:07 +0200

    Emacs

--- major-modes.el
+++ major-modes.el
@@ -10,7 +10,7 @@
 
 ;; Create links by id instead of 'file'. Makes destionation headings portable
 ;; and they can be changed and refiled.
-(setq org-id-link-to-org-use-id t)
+(setq org-id-link-to-org-use-id 'create-if-interactive)
 
 ;; Nice looking bullets
 (require 'org-bullets)
